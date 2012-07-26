3a4fe47e186f5b6a6e84e2878b9152028244317e
sam.sun <shalocfolmos@gmail.com>
2012-07-26 21:30:58 +0800

    commit

---
+++ Program/grails-app/conf/program/AppFilterFilters.groovy
@@ -0,0 +1,15 @@
+package program
+
+class AppFilterFilters {
+
+    def filters = {
+        all(controller:'(person)|(result)',action:'*',actionExclude:"(login)|(start)") {
+            before = {
+                if(session.user) {
+                    redirect(controller: "person", action: "start")
+                    return false
+                }
+            }
+        }
+    }
+}
