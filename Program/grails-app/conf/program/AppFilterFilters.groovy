8dd84c697eebec4486e8f21c2761e7936f9f19d0
sam.sun <shalocfolmos@gmail.com>
2012-07-27 18:24:44 +0800

    提交

--- Program/grails-app/conf/program/AppFilterFilters.groovy
+++ Program/grails-app/conf/program/AppFilterFilters.groovy
@@ -3,12 +3,23 @@ package program
 class AppFilterFilters {
 
     def filters = {
-        all(controller:'(person)|(result)',action:'*',actionExclude:"(login)|(start)") {
+        userResultFilter(controller:'result',action:'*') {
             before = {
+                if(actionName == "list" || actionName== "index" || actionName=="delete") {
+                    redirect(controller: "person", action: "start")
+                    return false;
+                }
                 if(!session.userId) {
                     redirect(controller: "person", action: "start")
                     return false
                 }
+            }
+        }
+
+        adminPersonFilter(controller: 'person',action:'*',excludeAction:"(login)|(start)") {
+            if(!session.superuser) {
+                redirect(controller: "person", action: "start")
+                return false
             }
         }
 
