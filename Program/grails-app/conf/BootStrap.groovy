4f9db5eed8b93eb969195d6c356ae0927d69762b
sam.sun <shalocfolmos@gmail.com>
2012-07-28 10:37:31 +0800

    提交

--- Program/grails-app/conf/BootStrap.groovy
+++ Program/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,9 @@
+import program.Person
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Person(username: "sam",password: "123",superuser: true,"总部").save()
     }
     def destroy = {
     }
